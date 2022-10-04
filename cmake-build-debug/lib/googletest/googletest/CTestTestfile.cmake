18891d06ffe9c45c888ec4b1ce73e645153a77fc
Christofer Daniel Chaves Pauth <cristoferchaves36@gmail.com>
2022-10-04 13:47:23 -0600

    Delete cmake-build-debug directory

--- cmake-build-debug/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ESCINF/Downloads/Proyecto-1---Estructuras-de-Datos/lib/googletest/googletest
-# Build directory: C:/Users/ESCINF/Downloads/Proyecto-1---Estructuras-de-Datos/cmake-build-debug/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
