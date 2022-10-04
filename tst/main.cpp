520891ece1c3417c18baa7f06995dcd69d6b7953
Christofer Daniel Chaves Pauth <cristoferchaves36@gmail.com>
2022-10-04 11:38:15 -0600

    Actualizando proyecto

--- tst/main.cpp
+++ tst/main.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //
 // Created by Maikol Guzman on 8/20/20.
 //
@@ -7,4 +8,15 @@
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
+=======
+//
+// Created by Maikol Guzman on 8/20/20.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+>>>>>>> 31344a19717938d0a7b13d079f50d8f153da8a85
 }
